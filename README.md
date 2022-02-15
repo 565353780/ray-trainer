639d126aeb9fd8c08334233ebf8a1854f43bb980
565353780 <565353780@qq.com>
2022-02-15 16:55:37 +0800

    finish trainer code

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# ray-trainer
+# Ray Trainer
+
+## Ubuntu Cluster Environment Prepare
+
+### Install CUDA and CUDNN
+
+```bash
+cuda 11.3
+```
+
+### Install Anaconda
+
+## Install
+
+### Create Same Python Environment
+
+```bash
+sudo apt install awscli
+conda create env -n ray python=3.8
+conda activate ray
+pip install "ray[rllib]" "gym[atari]" "gym[accept-rom-license]" atari_py opencv-python
+pip install torch==1.10.2+cu113 torchvision==0.11.3+cu113 torchaudio==0.10.2+cu113 \
+  -f https://download.pytorch.org/whl/cu113/torch_stable.html
+```
+
+run this on all pc and make sure that all versions listed are the same
+```bash
+pip list | grep -E "ray|torch|boto3"
+```
+
+## Start Cluster
+
+### Start Client
+
+run this on your client pc for running code later
+
+```bash
+conda activate ray
+ray start --head --port=<target-port>
+```
+
+### Start all Server
+
+run this on all your server pc
+
+```bash
+conda activate ray
+ray start --address=<target-address>
+```
+
+## Run your code
+
+add this to your code and run
+
+```bash
+ray.init(address="auto")
+```
+
+## Stop Server
+
+run this on your server pc
+
+```bash
+ray stop
+```
+
+## Enjoy it~
+
---
+++ ray_trainer.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import ray
+from ray import tune
+from ray.rllib.agents import ppo
+from ray.rllib.env.env_context import EnvContext
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.utils.framework import try_import_torch
+from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.tune.logger import pretty_print
+
+# import your gym env as MyEnv
+from WorldGenerateEnvironment import WorldGenerateEnvironment as MyEnv
+
+torch, nn = try_import_torch()
+
+class RayEnv(MyEnv):
+    def __init__(self, config: EnvContext):
+        MyEnv.__init__(self)
+        return
+
+class RayModel(TorchModelV2, nn.Module):
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        TorchModelV2.__init__(
+            self, obs_space, action_space, num_outputs, model_config, name
+        )
+        nn.Module.__init__(self)
+
+        self.torch_sub_model = TorchFC(
+            obs_space, action_space, num_outputs, model_config, name
+        )
+
+    def forward(self, input_dict, state, seq_lens):
+        input_dict["obs"] = input_dict["obs"].float()
+        fc_out, _ = self.torch_sub_model(input_dict, state, seq_lens)
+        return fc_out, []
+
+    def value_function(self):
+        return torch.reshape(self.torch_sub_model.value_function(), [-1])
+
+class RayTrainer(object):
+    def __init__(self):
+        self.run = "PPO"
+        self.as_test = True
+        self.config = {
+            "env": RayEnv,
+            "env_config": {
+                "test": True,
+            },
+            "model": {
+                "custom_model": RayModel,
+                "vf_share_layers": True,
+            },
+            "framework": "torch",
+            "num_gpus": 1,
+            "num_workers": 1,
+            "evaluation_num_workers": 1,
+            "evaluation_config": {
+                "render_env": True,
+            },
+        }
+        self.stop = {
+            "training_iteration": 50,
+            "timesteps_total": 100000,
+            "episode_reward_mean": 0.1,
+        }
+        return
+
+    def initRay(self):
+        ray.init()
+        return True
+
+    def manualTrain(self):
+        if self.run != "PPO":
+            print("[ERROR][RayTrainer::manualTrain]")
+            print("manual train only support PPO method!")
+            return False
+
+        ppo_config = ppo.DEFAULT_CONFIG.copy()
+        ppo_config.update(self.config)
+        ppo_config["lr"] = 1e-3
+
+        trainer = ppo.PPOTrainer(config=ppo_config, env=self.config["env"])
+
+        for _ in range(self.stop["training_iteration"]):
+            result = trainer.train()
+            print(pretty_print(result))
+            if (result["timesteps_total"] >= self.stop["timesteps_total"] or
+                result["episode_reward_mean"] >= self.stop["episode_reward_mean"]):
+                break
+
+        trainer.evaluate()
+        return True
+
+    def tuneTrain(self):
+        results = tune.run(self.run, config=self.config, stop=self.stop)
+
+        if self.as_test:
+            check_learning_achieved(results, self.stop["episode_reward_mean"])
+        return True
+
+    def closeRay(self):
+        ray.shutdown()
+        return True
+
+def demo_manual_train():
+    ray_trainer = RayTrainer()
+    ray_trainer.initRay()
+    ray_trainer.manualTrain()
+    ray_trainer.closeRay()
+    return True
+
+def demo_tune_train():
+    ray_trainer = RayTrainer()
+    ray_trainer.initRay()
+    ray_trainer.tuneTrain()
+    ray_trainer.closeRay()
+    return True
+
+if __name__ == "__main__":
+    demo_tune_train()
+
